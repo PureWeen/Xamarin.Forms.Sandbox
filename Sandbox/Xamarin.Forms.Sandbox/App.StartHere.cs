e62a81fdc1fedf0d74133f6d342762e4d3c07e8f
ShaneN <shane94@hotmail.com>
2019-07-29 12:09:12 -0600

    first commit

---
+++ Sandbox/Xamarin.Forms.Sandbox/App.StartHere.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using Xamarin.Forms.StyleSheets;
+
+namespace Xamarin.Forms.Sandbox
+{
+	public partial class App
+	{
+		void InitializeMainPage()
+		{
+			MainPage = new ShellPage();
+		}
+	}
+}
