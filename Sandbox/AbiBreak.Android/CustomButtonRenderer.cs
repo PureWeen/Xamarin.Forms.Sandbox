0049f425d9742f73a3d30e07215b8bfb31eeda9d
ShaneN <shane94@hotmail.com>
2019-08-05 13:08:11 -0600

    Add android project for ABI Break testing

---
+++ Sandbox/AbiBreak.Android/CustomButtonRenderer.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+using AbiBreak.Android;
+using Android.Content;
+using Android.Widget;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android.FastRenderers;
+
+[assembly: ExportRenderer(typeof(Xamarin.Forms.Button), typeof(CustomButtonRenderer))]
+namespace AbiBreak.Android
+{
+    public class CustomButtonRenderer : ButtonRenderer
+    {
+        public CustomButtonRenderer(Context context) : base(context)
+        {
+        }
+    }
+}
