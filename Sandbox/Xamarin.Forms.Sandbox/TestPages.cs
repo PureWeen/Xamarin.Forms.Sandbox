fe98d49da242292c6fc106ee6865b4326a55a1ba
Shane Neuville <shneuvil@microsoft.com>
2020-09-24 15:53:16 -0500

    - test pages

---
+++ Sandbox/Xamarin.Forms.Sandbox/TestPages.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Xamarin.Forms.Sandbox
+{
+    public abstract class TestMasterDetailPage : MasterDetailPage
+    {
+        protected TestMasterDetailPage()
+        {
+            Init();
+        }
+
+        abstract protected void Init();
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+        }
+    }
+
+    public abstract class TestTabbedPage : TabbedPage
+    {
+        protected TestTabbedPage()
+        {
+            Init();
+        }
+
+        abstract protected void Init();
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+        }
+    }
+
+    public abstract class TestNavigationPage : NavigationPage
+    {
+        protected TestNavigationPage()
+        {
+            Init();
+        }
+
+        abstract protected void Init();
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+        }
+    }
+}
